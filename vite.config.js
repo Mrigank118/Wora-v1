57a23cef4b1f90170fd94be53ee2b29c24b096b4
Ritika Jain <jainritikaa05@gmail.com>
2024-07-17 14:25:41 +0530

    14:25

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base:'/Wora-v1', // Replace with your GitHub repository name
+  plugins: [
+    react(),
+  ],
+  build: {
+    outDir: 'build', // Specify the output directory where Vite will put the built files
+    assetsDir: '.', // Ensure assets are correctly referenced relative to the base path
+    sourcemap: false, // Disable source maps to optimize build size
+  },
+});
